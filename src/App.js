9e0a014bb79dea70f5613a271ec6cb872cca9bf3
ArturKarimov <artur.karim@yandex.ru>
2021-08-19 17:35:17 +0300

    sorting function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,69 @@
-import React from 'react'
+import React, {useMemo, useState} from 'react'
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
+
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'JavaScript is a programming language'},
+        {id: 2, title: 'Python', body: 'My Python is a programming language'},
+        {id: 3, title: 'C++', body: 'WoW C++ is a programming language'}
+    ])
+
+    const [selectedSort, setSelectedSort] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+    }
+
+    const sortedPosts = useMemo(() => {
+        console.log('COMPLETED')
+        if (selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts
+    }, [selectedSort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+    }, [searchQuery, sortedPosts])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
     return (
-        <div className="App">
-            <div className="post">
-                <div className="post__content">
-                    <strong>1. JavaScript</strong>
-                    <div>
-                        JavaScript is a programming language
-                    </div>
-                </div>
-                <div className="post__btns">
-                    <button>Удалить</button>
-                </div>
+        <div className='App'>
+            <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <MyInput
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+                placeholder='Search...'
+            />
+            <div>
+                <MySelect
+                defaultValue='Sorting'
+                value={selectedSort}
+                onChange={sortPosts}
+                options={[
+                    {value: 'title', name: 'By name'},
+                    {value: 'body', name: 'By description'}
+                ]}
+                />
             </div>
+            {sortedAndSearchedPosts.length
+                ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'List of posts 1'}/>
+                : <h1 style={{textAlign: 'center'}}>Posts not found</h1>
+            }
         </div>
     )
 
